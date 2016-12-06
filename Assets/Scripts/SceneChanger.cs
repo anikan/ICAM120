d8dfdde67aa7756afc93011e734efb257220b945
Anish Kannan <ankannan@UCSD.EDU>
2016-12-05 17:20:28 -0800

    Made broke fixed my scene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public int sceneIncrementAmount;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEntsser(Collider other)
+    {
+        //Only head has camera
+        if (other.gameObject.GetComponent<Camera>())
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + sceneIncrementAmount);
+        }
+    }
+}
